f39bb5f54a57f555e95bb5cdc5d293c7057b4787
Vijay <vijucg@yahoo.com>
2019-07-30 14:52:44 +0530

    first commit of india

---
+++ india/src/test/java/com/example/vijay/india/IndiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vijay.india;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
